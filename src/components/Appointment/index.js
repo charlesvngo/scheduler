c41b3cf3f8d7b6c9cc00b2ec0ac17ea15d916b85
Charles Ngo <charlesngo37@gmail.com>
2022-05-18 00:09:27 -0700

    add additional components for the Appointment

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "components/Appointment/styles.scss";
+import "components/Appointment/header"
+
+const Appointment = (props) => {
+  return <article className="appointment"></article>
+}
+
+export default Appointment;
--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,9 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/header";
+import Empty from "components/Appointment/empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -135,3 +138,12 @@ storiesOf("InterviewerList", module)
     />
   ));
   
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm"/>)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
