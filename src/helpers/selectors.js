c4a728883c7e66f9604480179dc43414af1d81b5
Charles Ngo <charlesngo37@gmail.com>
2022-05-19 15:43:03 -0700

    helper to filter appointment data complete

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,5 @@
-const getAppointmentsForDay= (state, day) => {
-
+export const getAppointmentsForDay = (state, day) => {
+  const filteredDay = state.days.filter((days) => days.name === day)
+  const appointmentArray = filteredDay[0] ? filteredDay[0].appointments.map((appointmentId) => state.appointments[appointmentId]) : [];
+  return appointmentArray;
 }
-
-export default getAppointmentsForDay;
