1921bf84d55c863fe8b8628baea21177e2284eb5
Charles Ngo <charlesngo37@gmail.com>
2022-05-19 23:44:45 -0700

    cancel button now calls onCancel

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import "components/Appointment/Header"
 import Header from "components/Appointment/Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
@@ -11,23 +12,19 @@ const CREATE = "CREATE";
 
 const Appointment = (props) => {
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
-  let appointment;
-  if (mode === SHOW) {
-    appointment = <Show student={props.interview.student} interview={props.interview} />;
-  }
 
-  if (mode === EMPTY) {
-    appointment = <Empty />;
-  }
   return (
     <article className="appointment">
     <Header time={props.time} />
-    {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+    {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
     {mode === SHOW && (
       <Show
         student={props.interview.student}
         interview={props.interview}
       />
+    )}
+    {mode === CREATE && (
+      <Form interviewers={[]} onCancel={()=> back()}/>
     )}
     </article>
   )
