c4b7999ac1201790b7c8a3117444f763d80d3579
Charles Ngo <charlesngo37@gmail.com>
2022-05-25 17:37:27 -0700

    fix accidental import on appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
 import "components/Appointment/styles.scss";
-import "components/Appointment/Header"
 import Header from "components/Appointment/Header";
 import Show from "./Show";
 import Empty from "./Empty";
